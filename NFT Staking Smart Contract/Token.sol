29ce77f845d8bf06d0edf920f4d1bc25bdbcccbf
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-06-05 23:38:08 +0400

    Fixed some errors and optimized the contract

--- NFT Staking Smart Contract/Token.sol
+++ NFT Staking Smart Contract/Token.sol
@@ -3,15 +3,26 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Holder.sol";
 
+interface IERC721 {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
 
-contract MyToken is ERC20,ERC721Holder, Ownable {
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external;
+}
+
+contract MyToken is ERC20, Ownable {
      IERC721 public nft;
-     mapping(uint256 => address) public tokenOwnwerOf;
+     mapping(uint256 => address) public tokenOwnerOf;
      mapping(uint256 => uint256) public tokenStakedAt;
-     uint256 public EMISSION_RATE = 50 * 10 ^ decimals()) / 1 days;
+     uint256 public emission_rate = 50 * 10 ^ decimals() / 1 days;
 
 
     constructor(address _nft) ERC20("MyToken", "MTK") {
@@ -22,22 +33,26 @@ contract MyToken is ERC20,ERC721Holder, Ownable {
     //     _mint(to, amount);
     // }
 
-    function stake(uint256 tokenId) external{
-        nft.safeTransferFrom(msg.sender,address(this),token);
-        tokenOwnwerOf[tokenId] = msg.sender;
+    modifier isTokenValid(uint tokenId) {
+        require(tokenId >= 0, "Id doesn't exist");
+        _;
+    }
+
+    function stake(uint256 tokenId) external isTokenValid(tokenId){
+        nft.safeTransferFrom(msg.sender,address(this),tokenId);
+        tokenOwnerOf[tokenId] = msg.sender;
         tokenStakedAt[tokenId] = block.timestamp;
     }
 
-    function calculateTokens(uint256 tokenId) public view returns(uint256){
+    function calculateTokens(uint256 tokenId) public isTokenValid(tokenId) view returns(uint256){
         uint256 timeElapsed = block.timestamp - tokenStakedAt[tokenId];
-        return timeElapsed * EMISSION_RATE;
+        return timeElapsed * emission_rate;
     }
 
-    function unstake (uint256 tokenId)external{
-        require(tokenOwnwerOf[tokenId] == msg.sender,"You can't unstake ");
+    function unstake (uint256 tokenId)external isTokenValid(tokenId){
         _mint(msg.sender, calculateTokens(tokenId)); //minting the tokens for staking
         nft.transferFrom(address(this),msg.sender,tokenId);
-        delete tokenOwnwerOf[tokenId];
+        delete tokenOwnerOf[tokenId];
         delete tokenStakedAt[tokenId];
 
 
