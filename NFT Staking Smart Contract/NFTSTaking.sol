eaf29cd483310a2dac32428234176b376c6b19f3
Tevin-creator <isaactevin67@gmail..com>
2022-06-23 23:42:44 +0300

    added SPDX license Identifier in NFTStaking.sol file

--- NFT Staking Smart Contract/NFTSTaking.sol
+++ NFT Staking Smart Contract/NFTSTaking.sol
@@ -1,3 +1,8 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+ 
+
  import "@openzeppelin/contracts/access/Ownable.sol";
  import "@openzeppelin/contracts/utils/Address.sol";
  import "@openzeppelin/contracts/utils/math/SafeMath.sol";
