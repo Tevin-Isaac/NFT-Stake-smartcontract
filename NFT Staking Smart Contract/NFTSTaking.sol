9ba97707a1f4742a1490d2e4eb87e54cdb663e44
Tevin-creator <isaactevin67@gmail..com>
2022-06-23 23:15:42 +0300

    updated NFT smart contract and README.md file

---
+++ NFT Staking Smart Contract/NFTSTaking.sol
@@ -0,0 +1,235 @@
+ import "@openzeppelin/contracts/access/Ownable.sol";
+ import "@openzeppelin/contracts/utils/Address.sol";
+ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+ import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+ import '@openzeppelin/contracts/utils/math/Math.sol';
+ import "@openzeppelin/contracts/security/Pausable.sol";
+ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract NftStaking is Ownable, IERC721Receiver, ReentrancyGuard, Pausable {
+    using Address for address;
+    using SafeMath for uint;
+    using EnumerableSet for EnumerableSet.UintSet; 
+    
+    //addresses 
+    address nullAddress = 0x0000000000000000000000000000000000000000;
+    address public stakingDestinationAddress;
+    address public erc20Address;
+
+    //uint256's 
+    uint256 public expiration; 
+    //rate governs how often you receive your token
+    uint256 public rate; 
+
+    // unstaking possible after LOCKUP_TIME
+    uint public LOCKUP_TIME = 1 minutes;
+
+    // Contracts are not allowed to deposit, claim or withdraw
+    modifier noContractsAllowed() {
+        require(!(address(msg.sender).isContract()) && tx.origin == msg.sender, "No Contracts Allowed!");
+        _;
+    }
+
+    event RateChanged(uint256 newRate);
+    event ExpirationChanged(uint256 newExpiration);
+    event LockTimeChanged(uint newLockTime);
+  
+    // mappings 
+    mapping(address => EnumerableSet.UintSet) private _deposits;
+    mapping(address => mapping(uint256 => uint256)) public _depositBlocks;
+    mapping (address => uint) public stakingTime;
+
+    constructor(
+      address _stakingDestinationAddress,
+      uint256 _rate,
+      uint256 _expiration,
+      address _erc20Address
+    ) {
+        stakingDestinationAddress = _stakingDestinationAddress;
+        rate = _rate;
+        expiration = block.number + _expiration;
+        erc20Address = _erc20Address;
+        _pause();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+/* STAKING MECHANICS */
+
+    // Set a multiplier for how many tokens to earn each time a block passes.
+    function setRate(uint256 _rate) public onlyOwner() {
+      rate = _rate;
+      emit RateChanged(rate);
+    }
+
+    // Set this to a block to disable the ability to continue accruing tokens past that block number.
+    function setExpiration(uint256 _expiration) public onlyOwner() {
+      expiration = block.number + _expiration;
+      emit ExpirationChanged(expiration);
+    }
+
+    //Set Lock Time
+    function setLockTime(uint _lockTime) public onlyOwner() {
+      LOCKUP_TIME = _lockTime;
+      emit LockTimeChanged(LOCKUP_TIME);
+    }
+
+    //check deposit amount. 
+    function depositsOf(address account)
+      external 
+      view 
+      returns (uint256[] memory)
+    {
+      EnumerableSet.UintSet storage depositSet = _deposits[account];
+      uint256[] memory tokenIds = new uint256[] (depositSet.length());
+
+      for (uint256 i; i < depositSet.length(); i++) {
+        tokenIds[i] = depositSet.at(i);
+      }
+
+      return tokenIds;
+    }
+
+    function calculateRewards(address account, uint256[] memory tokenIds) 
+      public 
+      view 
+      returns (uint256[] memory rewards) 
+    {
+      rewards = new uint256[](tokenIds.length);
+
+      for (uint256 i; i < tokenIds.length; i++) {
+        uint256 tokenId = tokenIds[i];
+
+        rewards[i] = 
+          rate * 
+          (_deposits[account].contains(tokenId) ? 1 : 0) * 
+          (Math.min(block.number, expiration) - 
+            _depositBlocks[account][tokenId]);
+      }
+
+      return rewards;
+    }
+
+    //reward amount by address/tokenIds[]
+    function calculateReward(address account, uint256 tokenId) 
+      public 
+      view 
+      returns (uint256) 
+    {
+      // require(Math.min(block.number, expiration) > _depositBlocks[account][tokenId], "Invalid blocks");
+      return rate * 
+          (_deposits[account].contains(tokenId) ? 1 : 0) * 
+          (Math.min(block.number, expiration) - 
+            _depositBlocks[account][tokenId]);
+    }
+
+    //Update Account and Auto-claim 
+    function updateAccount(uint256[] calldata tokenIds) private {
+      uint256 reward; 
+      uint256 blockCur = Math.min(block.number, expiration);
+
+      for (uint256 i; i < tokenIds.length; i++) {
+        reward += calculateReward(msg.sender, tokenIds[i]);
+        _depositBlocks[msg.sender][tokenIds[i]] = blockCur;
+      }
+
+      if (reward > 0) {
+        require(IERC20(erc20Address).transfer(msg.sender, reward), "Could not transfer Reward Token!");
+      }
+    }
+
+    //Reward claim function
+    function claimRewards(uint256[] calldata tokenIds) external whenNotPaused noContractsAllowed nonReentrant(){
+      updateAccount(tokenIds);
+    }
+
+    //deposit function. 
+    function deposit(uint256[] calldata tokenIds) external whenNotPaused noContractsAllowed nonReentrant() {
+        require(msg.sender != stakingDestinationAddress, "Invalid address");
+        require(block.number < expiration, "Staking has finished, no more deposits!");
+        updateAccount(tokenIds);
+
+        for (uint256 i; i < tokenIds.length; i++) {
+            IERC721(stakingDestinationAddress).safeTransferFrom(
+                msg.sender,
+                address(this),
+                tokenIds[i],
+                ""
+            );
+
+            _deposits[msg.sender].add(tokenIds[i]);
+        }
+        stakingTime[msg.sender] = block.timestamp;
+    }
+
+    //withdrawal function.
+    function withdraw(uint256[] calldata tokenIds) external whenNotPaused noContractsAllowed nonReentrant() {
+
+    	require(block.timestamp.sub(stakingTime[msg.sender]) > LOCKUP_TIME, "You recently staked, please wait before withdrawing.");
+
+        updateAccount(tokenIds);
+
+        for (uint256 i; i < tokenIds.length; i++) {
+            require(
+                _deposits[msg.sender].contains(tokenIds[i]),
+                "Staking: token not deposited"
+            );
+
+            _deposits[msg.sender].remove(tokenIds[i]);
+
+            IERC721(stakingDestinationAddress).safeTransferFrom(
+                address(this),
+                msg.sender,
+                tokenIds[i],
+                ""
+            );
+        }
+    }
+
+    //withdraw without caring about Rewards
+    function emergencyWithdraw(uint256[] calldata tokenIds) external noContractsAllowed nonReentrant() {
+        require(block.timestamp.sub(stakingTime[msg.sender]) > LOCKUP_TIME, "You recently staked, please wait before withdrawing.");
+        
+        for (uint256 i; i < tokenIds.length; i++) {
+            require(
+                _deposits[msg.sender].contains(tokenIds[i]),
+                "Staking: token not deposited"
+            );
+
+            _deposits[msg.sender].remove(tokenIds[i]);
+
+            IERC721(stakingDestinationAddress).safeTransferFrom(
+                address(this),
+                msg.sender,
+                tokenIds[i],
+                ""
+            );
+        }
+    }
+
+    //withdrawal function.
+    function withdrawTokens() external onlyOwner {
+        uint256 tokenSupply = IERC20(erc20Address).balanceOf(address(this));
+        require(IERC20(erc20Address).transfer(msg.sender, tokenSupply), "Could not transfer Reward Token!");
+    }
+
+    // Prevent sending ERC721 tokens directly to this contract
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-These are two folders containing two NFT smart contracts.
-The NFT mint smart contract is a default smart contract where a user can mint NFTS of their choice.
-The NFT staking smart contract contains an ERC20 contract that users can stake tokens.It is dependent on the ERC721 nft smart contract.
+This system will consist of 3 contracts:
 
-This smart contracts were built in OpenZeppelin Contracts.
+An Erc20 token will be the reward token.
+An Erc721 will be the nft token.
+The staking contract will hold the staked nft and will allow claiming the rewards.
+
+
+
 click https://docs.openzeppelin.com/contracts/4.x/wizard to create your own NFT smart contracts too that you can give various features,access controls   and functions.
 
 The smart contracts were tested on https://remix.ethereum.org/  Remix IDE where you can compile the smart contracts and deploy them to the blockchain.
